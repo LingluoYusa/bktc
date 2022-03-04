ca94bb7dda9e449cfcff368c1c9e1cd56c801a56
LingluoYusa <87465762+LingluoYusa@users.noreply.github.com>
2022-03-04 19:51:20 +0800

    Update api.php

--- api.php
+++ api.php
@@ -1,6 +1,4 @@
 <?php
-header('Cache-Control:no-cache,must-revalidate');
-header('Pragma:no-cache');
 $img_array = glob("./sjapi/*.{gif,jpg,png,bmp,jpeg}",GLOB_BRACE);
 $img = array_rand($img_array); 
 header("location:.$img_array[$img]"); 
